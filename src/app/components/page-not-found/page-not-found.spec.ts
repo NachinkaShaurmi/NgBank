0b7ccdfd538e796f4070ef3afcd89a234617dbf5
PavelKonstantinovich <pashakonstantinovich@gmail.com>
2025-09-19 01:12:11 +0300

    refactor: add RouterTestingModule to test 404 page

--- src/app/components/page-not-found/page-not-found.spec.ts
+++ src/app/components/page-not-found/page-not-found.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { PageNotFound } from './page-not-found';
 
 describe('PageNotFound', () => {
@@ -8,9 +8,8 @@ describe('PageNotFound', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PageNotFound]
-    })
-    .compileComponents();
+      imports: [PageNotFound, RouterTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PageNotFound);
     component = fixture.componentInstance;
